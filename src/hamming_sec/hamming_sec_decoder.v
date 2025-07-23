223647c22820d8b1a0feb80264300ef62b157e4a
TalhaIsrar <talhaisrar02@gmail.com>
2025-07-23 18:52:08 +0200

    Added basic blocks

---
+++ src/hamming_sec/hamming_sec_decoder.v
@@ -0,0 +1,11 @@
+module hamming_sec_decoder(
+    input  [20:0] in_code;
+    output [15:0] out_data;
+    output error_corrected;
+);
+
+    always @(*) begin
+        
+
+    end
+endmodule
---
+++ src/hamming_sec/hamming_sec_encoder.v
@@ -0,0 +1,10 @@
+module hamming_sec_encoder(
+    input  [15:0] input_data;
+    output [20:0] output_code;
+);
+
+    always @(*) begin
+        
+
+    end
+endmodule
