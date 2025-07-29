ee7e2464b2843995929a88049fe1799e29db74c4
TalhaIsrar <talhaisrar02@gmail.com>
2025-07-29 21:38:57 +0200

    Added CRC4 Encoder using LFSR

--- src/crc_code/crc_code_encoder.v
+++ src/crc_code/crc_code_encoder.v
@@ -11,7 +11,7 @@ module crc_code_encoder(
     output [3:0] addr_out
 );
 
-    reg [7:0] data_reg, lsfr_shift_reg;
+    reg [7:0] data_reg, data_shift_reg;
     reg [3:0] addr_reg;
 
     reg [3:0] lfsr;
@@ -34,30 +34,31 @@ module crc_code_encoder(
     // Shift register for LFSR
     always @(posedge clk or posedge rst) begin
         if (rst)
-            lsfr_shift_reg  <= 8'b00000000;
+            data_shift_reg  <= 8'b00000000;
         else begin
             if (load)
-                lsfr_shift_reg  <= data_in;
+                data_shift_reg  <= data_in;
             else if (shift_en)
-                lsfr_shift_reg  <= {lsfr_shift_reg[6:0] , 1'b0};
+                data_shift_reg  <= {data_shift_reg[6:0] , 1'b0};
         end 
     end
 
     // MSB of input to pass to LFSR
-    assign lsfr_input = lsfr_shift_reg[7]
+    assign lsfr_input = data_shift_reg[7];
     
     // LFSR to calculate the remainder
     always @(posedge clk or posedge rst) begin
         if (rst)
             lfsr <= 4'b0000;
         else if (shift_en) begin
+            // Implements CRC-4 with polynomial x^4 + x + 1
             lfsr <= {lfsr[2:1] , lfsr[3] ^ lfsr[0], lfsr[3] ^ lsfr_input};
         end 
     end
 
     // Assign the outputs
-    assign addr_out = data_reg;
+    assign addr_out = addr_reg;
     assign data_out[11:4] = data_reg;
-    assign data_out[3:0] = lsfr_shift_reg;
+    assign data_out[3:0] = lfsr;
 
 endmodule
---
+++ tb/crc_tb/crc_controller_encoder_tb.v
@@ -0,0 +1,65 @@
+`timescale 1ns/1ps
+
+module crc_controller_encoder_tb();
+
+    reg clk;
+    reg rst;
+
+    reg write;
+
+    wire shift_en;
+    wire load_en;
+    wire write_mem_en;
+    wire write_mem_busy;
+
+    // Instantiate DUT
+    crc_code_controller dut (
+        .clk(clk),
+        .rst(rst),
+        .write(write),
+        .shift_en(shift_en),
+        .load_en(load_en),
+        .write_mem_en(write_mem_en),
+        .write_mem_busy(write_mem_busy)
+    );
+
+
+    // Clock generation: 10ns period
+    initial clk = 0;
+    always #5 clk = ~clk;
+
+    initial begin
+        rst = 1;
+        #20
+
+        rst = 0;
+        write = 0;
+        $display("\nReset complete.");
+        #20
+
+        write = 1;
+        #10
+
+        write = 0;
+        #40
+
+        write = 1;
+        #20
+
+        write = 0;
+        #300
+
+        write = 1;
+        #10
+
+        write = 0;
+        #200
+
+        // Done
+        $display("\nSimulation complete.");
+        #20;
+        $finish;
+
+    end
+
+endmodule
