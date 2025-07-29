c62eb2e75ca51833993060580cea0b4544f6fa88
TalhaIsrar <talhaisrar02@gmail.com>
2025-07-29 22:26:01 +0200

    Modified Memory to add read write to different address support

--- src/crc_code/mem_crc_code.v
+++ src/crc_code/mem_crc_code.v
@@ -2,19 +2,27 @@ module mem_crc_code (
     input         clk,
     input         rst,
     input         wr_en,
-    input  [3:0]  addr,
-    input  [11:0] data_in,
-    output [11:0] data_out
+    input  [3:0]  write_addr,
+    input  [3:0]  read_addr,
+    input  [11:0] write_data,
+    output [11:0] read_data
 );
 
     reg [11:0] mem [0:15];
 
-    assign data_out = mem[addr];
-
+    // Write to memory
     always @(posedge clk) begin
-        if (wr_en) begin
-            mem[addr] <= data_in;
+        if (write_en) begin
+            mem[write_addr] <= write_data;
         end
+    end
+
+    // If read and write address are same pass input data to output
+    always @(*) begin
+        if (write_addr == read_addr)
+            read_data = write_data;
+        else
+            read_data = mem[addr];
     end
 
 endmodule
